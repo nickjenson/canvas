12902d5615f2ef1aacc2e43004e89f59ea621dd6
Nick Jenson <nickjenson@gmail.com>
2017-07-06 08:39:07 -0600

    updated .hide method to reflect .remove instead

--- branding/hide-not-admin/hide-not-admin.js
+++ branding/hide-not-admin/hide-not-admin.js
@@ -3,8 +3,8 @@
 $(document).ready(function() {
   if (window.location.pathname == "/courses/" + ENV.COURSE_ID + "/settings") {
     if($.inArray("admin", ENV.current_user_roles) == -1) {
-        $("#example_id").hide();
-        //$(".example_class").hide();
+        $("#example_id").remove();
+        //$(".example_class").remove();
   	}
   }
 });

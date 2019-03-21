847f0785560385b9e5369054893db8a57e3ecf52
Nick Jenson <nickjenson@gmail.com>
2019-03-21 15:19:17 -0600

    added slop as an option parser

--- api/list-avatars/list-avatars.rb
+++ api/list-avatars/list-avatars.rb
@@ -1,77 +1,79 @@
-require 'csv'
-require 'json'
+require 'slop'
+require 'yaml'
 require 'bearcat'
-require 'typhoeus'
 require 'ruby-progressbar'
+require 'csv'
 
-### CHANGE THESE VALUES
+env = ''
+version = '0.0.2'
+script = 'list-avatars.rb'
 
-domain = '' # e.g. 'domain' in 'https://domain.instructure.com'
-token = '' # api token for account admin user
-env = '' # leave blank for production, or use test or beta.
-
-# =======================
-# Do not edit from here unless you know what you're doing.
+opts = Slop.parse do |opts|
+  opts.banner = 'Usage: script.rb [options]'
+  opts.string '-t', '--token', 'api token'
+  opts.string '-d', '--domain', 'domain'
+  opts.string '-c', '--client', 'client.yaml' do
+    yaml = "#{opts[:client]}/#{opts[:client]}.yaml"
+    opts = load_file(yaml)
+    raise 'Client does not exist!' unless File.exist?(yaml)
+  end
+  opts.on '-h', '--help', 'help' do
+    puts "#{script} v.#{version}", opts
+    exit
+  end
+end
 
 env != '' ? env << '.' : env
-base_url = "https://#{domain}.#{env}instructure.com"
+url = "https://#{opts[:domain]}.#{env}instructure.com"
 
-def fetch_users(base_url, token, domain)
+if [opts[:domain], opts[:token]].any?(&:nil?)
+  raise 'Error: Missing one or more required fields'
+end
+
+def fetch_users(url, opts)
   puts 'Fetching users...'
-
-  client = Bearcat::Client.new(token: token, prefix: base_url)
+  client = Bearcat::Client.new(token: opts[:token], prefix: url)
   all_users = client.list_users('self').all_pages!.to_a
-
   user_ids = []
+
   all_users.each do |user|
     user_ids << user['id']
   end
-  fetch_avatar_urls(client, user_ids, domain)
+  fetch_avatar_urls(client, user_ids, opts)
 end
 
-def fetch_avatar_urls(client, user_ids, domain)
+def fetch_avatar_urls(client, user_ids, opts)
   progressbar = ProgressBar.create(total: user_ids.count)
   users_with_avatars = []
 
   user = {}
   user_ids.each do |id|
-    x = client.user_profile(id)
-    user[:id]          = x['id']
-    user[:name]        = x['name']
-    user[:short_name]  = x['short_name']
-    user[:sis_user_id] = x['sis_user_id']
-    user[:avatar_url]  = x['avatar_url']
-    users_with_avatars << x
-
+    id = client.user_profile(id)
+    user[:id]          = id['id']
+    user[:name]        = id['name']
+    user[:short_name]  = id['short_name']
+    user[:sis_user_id] = id['sis_user_id']
+    user[:avatar_url]  = id['avatar_url']
+    users_with_avatars << id
     progressbar.increment
   end
-
   puts 'Preparing CSV...'
-  print_to_csv(users_with_avatars, domain)
+  print_to_csv(users_with_avatars, opts)
 end
 
-def create_csv(domain)
-  headers = "id,name,short_name,sis_user_id,avatar_url\n"
-
-  File.open("./#{domain}_avatar-list.csv", 'w'){ |x| x.write(headers) }
-end
-
-def print_to_csv(users, domain)
+def print_to_csv(users_with_avatars, opts)
   headers = %w[]
-  users.each do |user|
-    CSV.open("./#{domain}_avatar-list.csv", 'a') do |csv|
+  CSV.open("./#{opts[:domain]}_avatar-list.csv", 'wb') do |csv|
+  users_with_avatars.each do |user|
       row = CSV::Row.new(headers, [])
       row['id']              = user['id']
       row['name']            = user['name']
       row['short_name']      = user['short_name']
       row['sis_user_id']     = user['sis_user_id']
       row['avatar_url']      = user['avatar_url']
-
       csv << row
     end
   end
   puts 'Done'
 end
-
-create_csv(domain)
-fetch_users(base_url, token, domain)
+fetch_users(url, opts)
